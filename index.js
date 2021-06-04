368430bfe8d44a718a8ed6b6164840d7910131f8
McCray <tubach75@hotmail.com>
2021-06-04 09:51:53 -0400

    commit tree 4ecdf7609a17e40ff2914f61a1af49ba33e52870

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books').then(function(response){
+    return response.json()
+  }) .then(function(json){
+    renderBooks(json);
+  })
   
 }
 
